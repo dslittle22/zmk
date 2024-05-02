8191ecd7000713db3feb2fa27365eb8a242506a9
Danny Little <dslittle22@gmail.com>
2024-05-02 16:04:37 +0000

    make require-prior-idle-ms shorter

--- build_combined.sh
+++ build_combined.sh
@@ -5,11 +5,9 @@ if [[ $(pwd) = "/workspaces/zmk" ]]; then
     cd app
 fi
 
-
-
 west build -p -d build/left -b glove80_lh & \
     west build -p -d build/right -b glove80_rh & \
     wait;
 
 mkdir -p build/combined && \
-    cat build/left/zephyr/zmk.uf2 build/right/zephyr/zmk.uf2 > build/combined/glove80.uf2
+    cat /workspaces/zmk/app/build/left/zephyr/zmk.uf2 /workspaces/zmk/app/build/right/zephyr/zmk.uf2 > /workspaces/zmk/app/build/combined/zmk.uf2
